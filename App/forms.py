1a490d05f5ff6e59473f7279bd736ab7f2d50f07
ErickYar <erickyb1985@gmail.com>
2023-12-04 06:38:58 -0500

    poniendo las opciones en el atributo cursos

--- App/forms.py
+++ App/forms.py
@@ -1,12 +1,27 @@
 from django import forms
 
+LISTA_CURSOS=(
+    ("Python","Python"),
+    ("Django","Django"),
+    ("PHP","PHP"),
+    ("Java","Java"),
+    ("SQLite","SQLite"),
+    ("JavaScript","JavaScript"),
+
+    
+
+
+
+
+)
+
 # clase definir los datos del alumnos
 
 class Alumno(forms.Form):
     #definir los datos
     codigo=forms.CharField()
     nombre=forms.CharField()
-    curso=forms.ChoiceField()
+    curso=forms.ChoiceField(choices=LISTA_CURSOS)
     parcial=forms.IntegerField()
     practica=forms.IntegerField()
     final=forms.IntegerField()
